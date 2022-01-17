91db893221294fe81b96924c689c74007f2e7490
Srinivasan Elangovan <srininearyou@gmail.com>
2022-01-17 09:01:32 +0530

    Test commit

--- src/main/java/com/java/knapsack/Application.java
+++ src/main/java/com/java/knapsack/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(value ="com" )
+
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
